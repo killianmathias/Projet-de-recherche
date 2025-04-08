1a888677475407c8575ae6e5cfb4188288425eb2
mgirar10 <mattheo.girardclos@edu.univ-fcomte.fr>
2025-04-08 10:52:27 +0200

    gestion des hitbox avec la structuration

--- Code/entite/player.py
+++ Code/entite/player.py
@@ -5,14 +5,14 @@ class Player(Entity):
     
     def __init__(self, x, y, scale, speed, gravity):
         img = pygame.image.load('Code/img/player.png')
-        Entity.__init__(self, x, y, int(img.get_width() * scale), int(img.get_height() * scale), 'Code/img/player.png')
+        self.width = int(img.get_width() * scale)
+        self.height = int(img.get_height() * scale)
+        Entity.__init__(self, x, y, self.width, self.height, 'Code/img/player.png')
         self.speed = speed
         self.jump = False
         self.vel_y = 0
         self.in_air = True
         self.gravity = gravity
-        self.moving_left = False
-        self.moving_right = False
 
         
     def draw(self, screen):
@@ -22,30 +22,44 @@ class Player(Entity):
         dx = 0
         dy = 0
         
-        if self.moving_left:
-            dx = -self.speed
-        if self.moving_right:
-            dx = self.speed
-                
-        if self.jump == True:
+        key = pygame.key.get_pressed()
+        if key[pygame.K_SPACE] and self.in_air == False:
             self.vel_y = -11
-            self.jump = False
             self.in_air = True
+        if key[pygame.K_q]:
+            dx -= self.speed
+        if key[pygame.K_d]:
+            dx += self.speed
             
-        if self.in_air:
-            self.vel_y += self.gravity
-            if self.vel_y > 10:
-                self.vel_y = 10
+        self.vel_y += self.gravity
+        if self.vel_y > 10:
+            self.vel_y = 10
         dy += self.vel_y
             
+            
+        self.in_air = True
+        for bloc in blocs:
+            #check for collision in x direction
+            if bloc.rect.colliderect(self.rect.x + dx, self.rect.y, self.width, self.height):
+                dx = 0
+            #check for collision in y direction
+            if bloc.rect.colliderect(self.rect.x, self.rect.y + dy, self.width, self.height):
+                #check if below the ground i.e. jumping
+                if self.vel_y < 0:
+                    dy = bloc.rect.bottom - self.rect.top
+                    self.vel_y = 0
+                #check if above the ground i.e. falling
+                elif self.vel_y >= 0:
+                    dy = 0
+                    self.rect.bottom = bloc.rect.top
+                    self.vel_y = 0
+                    self.in_air = False
+                    
         if self.rect.bottom + dy > 400:
             dy = 400 - self.rect.bottom
             self.in_air = False
             self.vel_y = 0
-
-        for bloc in blocs:
-            if self.rect.colliderect(bloc.rect):
-                print('colision')
+            
 
         self.rect.x += dx
         self.rect.y += dy
--- Code/game.py
+++ Code/game.py
@@ -11,11 +11,14 @@ class Game():
         self.clock = pygame.time.Clock()
         self.running = True
         
-        self.player = Player(200, -2000, 0.1, 5, 0.7)
+        self.player = Player(200, 200, 0.1, 5, 0.7)
 
-        bloc = Bloc(600, 375, 50)
         self.blocs = pygame.sprite.Group()
-        self.blocs.add(bloc)
+        for i in range(10):
+            bloc = Bloc(100 + i * 50, 375, 50)
+            self.blocs.add(bloc)
+        
+        
         
         
     def run(self):
@@ -34,23 +37,9 @@ class Game():
                     self.running = False
                 
                 if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_q:
-
-                        self.player.moving_left = True
-                    if event.key == pygame.K_d:
-                        self.player.moving_right = True
-                    if event.key == pygame.K_SPACE :
-                        self.player.jump = True
                     if event.key == pygame.K_ESCAPE:
                         self.running = False
                         
-                if event.type == pygame.KEYUP:
-                    if event.key == pygame.K_q:
-                        self.player.moving_left = False
-                    if event.key == pygame.K_d:
-                        self.player.moving_right = False
-
-
             self.player.update(self.blocs)
 
             pygame.display.update()
